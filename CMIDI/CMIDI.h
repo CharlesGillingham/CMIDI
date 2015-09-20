b2832759f481962bf9748a874ae5a4ee8d5c602b
charlesgillingham <cgillingham1@mac.com>
2015-09-20 12:57:51 -0700

    Initial Commit

---
+++ CMIDI/CMIDI.h
@@ -0,0 +1,19 @@
+//
+//  CMIDI.h
+//  CMIDI
+//
+//  Created by CHARLES GILLINGHAM on 9/20/15.
+//  Copyright (c) 2015 CharlesGillingham. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CMIDI.
+FOUNDATION_EXPORT double CMIDIVersionNumber;
+
+//! Project version string for CMIDI.
+FOUNDATION_EXPORT const unsigned char CMIDIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMIDI/PublicHeader.h>
+
+
